aed04574d4403a55c97d27b05c41430163764a94
Lanzz Cala <lanzzcala@gmail.com>
2025-04-10 20:28:33 -0700

    Comment steps to start project. Still need to add more

---
+++ main.py
@@ -0,0 +1,10 @@
+#Greeting message to intro Pomodoro timer
+#Have a start command for timer
+#Need a timer that visually counts down at 25 mins
+#Timer stops at 25 mins and starts 5 min rest timer
+#Display pomodoro is starting
+#Alarm/notification for starting
+#Display pomodoro is ending
+#Alarm/notification for ending
+#Pause timer feature
+#Display feature for repeat
