ba710cc28e4cbf1c4d9c7b7b14edcf95629f579a
Lanzz Laptop <calap.dev@gmail.com>
2025-04-12 16:54:25 -0700

    Connect intro function to start function

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import time 
 import os
-#Display timer in MM:SS format
-#Have delay countdown to start pomodoro timer
-#Need a timer that visually counts down at 25 mins
-#Timer stops at 25 mins and starts 5 min rest timer
+
+#Timer stops at 25 mins
+# Timer starts 5 min rest timer
 #Display pomodoro is starting
 #Alarm/notification for starting
 #Display pomodoro is ending
@@ -18,32 +17,39 @@ def pomodoro_intro():
         start = input("Welcome to your 25 minute Pomodoro session. Ready to start? (Yes/No) ").capitalize()
         if start == "No":
             print("No problem! Relaunch again when you're ready to start.")
+            return start
             break
         elif start != "Yes" and start != "No":
-            print("Invalid response. Please try again.")
-            print (start)
+            print(f"You entered {start} which is an invalid response. Please try again.")
             continue
         else:
             print ("Let's start.")
+            return start
 
-def pomodoro_start():
-    mins = 1
-    seconds = 3
-    print (f"Starting at {mins}:{seconds:02}")
-    while True:
-        time.sleep(1)
-        os.system("cls")
-        if mins != 0 and seconds == 0:
-            mins -= 1
-            seconds = 59
-            print (f"{mins}:{seconds:02}")
-        elif seconds != 0:
-            seconds -= 1
-            print (f"{mins}:{seconds:02}")
-        elif mins == 0 and seconds == 0:
-            print ("Done!")
-            break
+def pomodoro_start(start):
+    if start == "No":
+        return None
+    else:
+        #Need a timer that visually counts down at 25 mins
+        mins = 1
+        seconds = 3
+        #Display timer in MM:SS format
+        print (f"Starting at {mins}:{seconds:02}")
+        while True:
+            #Have delay countdown to start pomodoro timer
+            time.sleep(1)
+            os.system("cls")
+            if mins != 0 and seconds == 0:
+                mins -= 1
+                seconds = 59
+                print (f"{mins}:{seconds:02}")
+            elif seconds != 0:
+                seconds -= 1
+                print (f"{mins}:{seconds:02}")
+            elif mins == 0 and seconds == 0:
+                print ("Done!")
+                break
 
 
 
-pomodoro_start()
+pomodoro_start(pomodoro_intro())
