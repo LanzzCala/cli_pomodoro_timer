c69495e580bb6e16474c77cbefe9f7cb26ac7c46
Lanzz Cala <lanzzcala@gmail.com>
2025-04-14 21:27:55 -0700

    Start 5 min break function. Attempt to connect timer and break

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import time
 import os
 
 #Timer stops at 25 mins
-# Timer starts 5 min rest timer
-#Display pomodoro is starting
+#Timer starts 5 min rest timer
 #Alarm/notification for starting
 #Display pomodoro is ending
 #Alarm/notification for ending
@@ -39,24 +38,46 @@ def pomodoro_start(start):
         return None
     else:
         #Need a timer that visually counts down at 25 mins
-        mins = 25
-        seconds = 0
-        #Display timer in MM:SS format
-        print ("Starting your Pomodoro timer!")
+        mins = 0
+        seconds = 5
+        timer_start = True
+        print ("Starting your Pomodoro timer!") #Display pomodoro is starting
         time.sleep(2)
-        while True:
-            #Have delay countdown to start pomodoro timer
+        while timer_start == True:
             time.sleep(1)
             os.system("cls")
             if mins != 0 and seconds == 0:
                 mins -= 1
                 seconds = 59
-                print (f"{mins}:{seconds:02}")
+                print (f"{mins}:{seconds:02}")#Display timer in MM:SS format
             elif seconds != 0:
                 seconds -= 1
-                print (f"{mins}:{seconds:02}")
+                print (f"{mins}:{seconds:02}")#Display timer in MM:SS format
             elif mins == 0 and seconds == 0:
                 print ("Done!")
+                timer_start == False
+        pomodoro_5_min_break(timer_start)
+
+
+def pomodoro_5_min_break(timer_start):
+    if timer_start == False:       
+        print("Break time!")
+        mins = 5
+        seconds = 0
+        timer_start == True
+        while timer_start == True:
+            time.sleep(1)
+            os.system("cls")
+            if mins != 0 and seconds == 0:
+                mins -= 1
+                seconds = 59
+                print (f"{mins}:{seconds:02}")#Display timer in MM:SS format
+            elif seconds != 0:
+                seconds -= 1
+                print (f"{mins}:{seconds:02}")#Display timer in MM:SS format
+            elif mins == 0 and seconds == 0:
+                print ("Back to work")
+                timer_start == False
                 break
 
 
